6a7107f8ebf427ab6e2122114e8cfabc9dcc3a33
yujin9286 <55122284+yujin9286@users.noreply.github.com>
2019-12-11 23:39:14 +0900

    bookcrawling(include preprocessing).py

---
+++ bookcrawling.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+
+book_title=[]
+book_rating=[]
+book_price=[]
+
+for page_end in range(1, 1500):
+    url = 'https://series.naver.com/ebook/categoryProductList.nhn?categoryTypeCode=all&page=' + str(page_end)
+    page=urlopen(url)
+    soup=BeautifulSoup(page,"html.parser")
+    book_title.extend([soup.find_all('div','cont')[n].h3.a.string for n in range(0,25)])
+    book_rating.extend([soup.find_all('em','score_num')[n].string for n in range(0,25)])
+    book_price.extend([soup.find_all('p','price')[n].strong.string.replace(",","").replace("무료","0") for n in range(0,25)])
+
+
+data=pd.DataFrame({'title':book_title,'rating':book_rating,'price':book_price})
+data.to_csv("bookcrawling.csv")
